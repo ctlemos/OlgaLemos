2ab1fc2d31f62aae170421b31dfcc5b8efa6e486
ctlemos <catarinatlemost@gmail.com>
2025-03-21 21:50:38 +0000

    rwd added to services page + css cleaning

--- src/pages/Servicos.jsx
+++ src/pages/Servicos.jsx
@@ -1,7 +1,7 @@
 import { products } from '../js/servicosData';
 import { useState, useRef} from 'react';
 import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
-import './styles/Servicos.css';
+import './styles/servicos.css';
 
 
 const Servicos = () => {
